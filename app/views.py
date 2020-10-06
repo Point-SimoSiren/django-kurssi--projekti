f2b43d71e9f5c1f722ae4a7bcf74c626c5490a7d
Simo Siren <simo.siren@stud.pointcollege.fi>
2020-10-06 12:11:01 +0300

    navigointi ok sivujen välillä

--- app/views.py
+++ app/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from .models import Supplier, Product
 
 def landingview(request):
     return render (request, "landingpage.html")
+
+def supplierlistview(request):
+    supplierlist = Supplier.objects.all()
+    context = {'suppliers': supplierlist}
+    return render (request, "suppliers.html", context)
+
+def productlistview(request):
+    productlist = Product.objects.all()
+    context = {'products': productlist}
+    return render (request, "products.html", context)
