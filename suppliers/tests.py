e63981e000a4927feeb351755b2fde942e25f892
Simo Siren <simo.siren@stud.pointcollege.fi>
2020-10-12 22:00:05 +0300

    tests bit more completed

--- suppliers/tests.py
+++ suppliers/tests.py
@@ -5,15 +5,22 @@ from suppliers.laskin import plus, plus_complicated
 
 class LaskinTests(TestCase):
     def test_plus(self):
-        # testaa että numerot lasketaan yhteen
+        # testaa että numerot lasketaan yhteen oikein
         self.assertEqual(plus(7, 2), 9)
-        self.assertEqual(plus(7.1, 2.7), 9.8)
+        self.assertEqual(plus(7.20, 2.70), 9.90)
+        self.assertEqual(plus(-7, 2), -5)
 
     def test_plus_complicated(self):
-        # testing adding with conditional if clause
+        # testaa ehdollisen yhteenlaskun toimivuus
         self.assertEqual(plus_complicated(7, 2), 9)
         self.assertEqual(plus_complicated(2, 8), 8)
 
     @unittest.expectedFailure
-    def test_plus_should_fail(x, y):
-        self.assertEqual(plus('7', 2), 9)
+    def test_plus_should_fail(self):
+        self.assertEqual(plus(7, '2'), '9')
+        self.assertEqual(plus(7, '2'), 9)
+        self.assertEqual(plus('7', '2'), '9')
+        self.assertEqual(plus(7.1, 2.1), 9.2)
+
+    # TDD - Test Driven Development
+    '''testi, toiminto, refaktotointi, testi, ... uudistus, testi'''
