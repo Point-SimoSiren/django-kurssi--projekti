545c1f0694533e732d7b91229d06873525957985
Simo Siren <simo.siren@stud.pointcollege.fi>
2020-10-12 12:25:29 +0300

    product edit done

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import landingview, supplierlistview, productlistview
+from .views import landingview, supplierlistview, productlistview, addsupplier, addproduct, deletesupplier, deleteproduct, edit_product_get, edit_product_post
 
 urlpatterns = [
     path('', landingview),
     path('suppliers/', supplierlistview),
+    path('add-supplier/', addsupplier),
+    path('delete-supplier/<int:iidee>/',deletesupplier),
     path('products/', productlistview),
+    path('add-product/', addproduct),
+    path('delete-product/<int:iidee>/',deleteproduct),
+    path('edit-product-get/<int:iidee>/', edit_product_get),
+    path('edit-product-post/<int:iidee>/', edit_product_post),
 ]
--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Supplier, Product
 
 def landingview(request):
@@ -9,7 +9,51 @@ def supplierlistview(request):
     context = {'suppliers': supplierlist}
     return render (request, "suppliers.html", context)
 
+
+def addsupplier(request):
+    a = request.POST['companyname']
+    b = request.POST['contactname']
+    c = request.POST['address']
+    d = request.POST['phone']
+    e = request.POST['email']
+    f = request.POST['country']
+    Supplier(companyname = a, contactname = b, address = c, phone = d, email = e, country = f).save()
+    return redirect(request.META['HTTP_REFERER'])
+
+
+def deletesupplier(request,iidee):
+    Supplier.objects.filter(id = iidee).delete()
+    return redirect(request.META['HTTP_REFERER'])
+
+
+# PRODUCTS HANDLING AND VIEW FUNCTIONS
+
 def productlistview(request):
     productlist = Product.objects.all()
     context = {'products': productlist}
     return render (request, "products.html", context)
+
+def addproduct(request):
+    a = request.POST['productname']
+    b = request.POST['packagesize']
+    c = request.POST['unitprice']
+    d = request.POST['unitsinstock']
+    e = request.POST['companyname']
+    Product(productname = a, packagesize = b, unitprice = c, unitsinstock = d, companyname = e).save()
+    return redirect(request.META['HTTP_REFERER'])
+
+def deleteproduct(request,iidee):
+    Product.objects.filter(id = iidee).delete()
+    return redirect(request.META['HTTP_REFERER'])
+
+def edit_product_get(request, iidee):
+    product = Product.objects.filter(id = iidee)
+    context = {'product': product}
+    return render (request,"edit_product.html",context)
+
+def edit_product_post(request, iidee):
+    item = Product.objects.get(id = iidee)
+    item.unitprice = request.POST['unitprice']
+    item.unitsinstock = request.POST['unitsinstock']
+    item.save()
+    return redirect(productlistview)
