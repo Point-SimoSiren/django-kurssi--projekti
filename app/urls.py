8e1e221c68a76e02ebb06be4efc28d687eb7fb84
Simo Siren <simo.siren@stud.pointcollege.fi>
2021-05-02 21:16:33 +0300

    listaus testit

--- app/urls.py
+++ app/urls.py
@@ -1,14 +1,27 @@
 from django.urls import path
-from .views import landingview, supplierlistview, productlistview, addsupplier, addproduct, deletesupplier, deleteproduct, edit_product_get, edit_product_post
+from .views import supplierlistview, productlistview, addsupplier, addproduct, deletesupplier, deleteproduct, landingview, edit_product_get, edit_product_post, edit_supplier_get, edit_supplier_post, products_filtered, loginview, login_action, logout_action
 
 urlpatterns = [
-    path('', landingview),
+    # LANDING PAGE AFTER LOGIN
+    path('landing/', landingview),
+
+    # Loginview and authentication method
+    path('', loginview),
+    path('login/', login_action),
+    path('logout/', logout_action),
+
+    # suppliers urls
     path('suppliers/', supplierlistview),
     path('add-supplier/', addsupplier),
-    path('delete-supplier/<int:iidee>/',deletesupplier),
+    path('delete-supplier/<int:iidee>/', deletesupplier),
+    path('edit-supplier-get/<int:iidee>/', edit_supplier_get),
+    path('edit-supplier-post/<int:iidee>/', edit_supplier_post),
+    
+    # products urls
     path('products/', productlistview),
-    path('add-product/', addproduct),
-    path('delete-product/<int:iidee>/',deleteproduct),
+    path('add-product/',addproduct),
+    path('delete-product/<int:iidee>/', deleteproduct),
     path('edit-product-get/<int:iidee>/', edit_product_get),
     path('edit-product-post/<int:iidee>/', edit_product_post),
+    path('products-by-supplier/<int:iidee>/', products_filtered),
 ]
--- suppliers/tests.py
+++ suppliers/tests.py
@@ -1,7 +1,44 @@
 from django.test import TestCase
 import unittest
-
 from suppliers.laskin import plus, plus_complicated
+from app.models import Supplier, Product
+
+from app.views import supplierlistview, productlistview
+from django.test import Client
+from django.urls import reverse
+client = Client()
+
+
+class ListMethodTests(TestCase):
+    def test_listing_products(self):
+        '''Call to product list url returns statuscode 200'''
+        response = client.get(reverse(productlistview))
+        self.assertEqual(response.status_code, 200)
+     def test_listing_suppliers(self):
+         '''Call to supplier list url returns statuscode 200'''
+        response = client.get(reverse(supplierlistview))
+        self.assertEqual(response.status_code, 200)
+
+
+class SupplierModelTests(TestCase):
+    def setUp(self):
+        Supplier.objects.create(companyname="Test company", contactname="Jaakko Kulta", address="Kultatie 1", phone="12345567", email="jaakko@kulta.fi", country="Finland")
+        
+    def test_added_supplier_exists(self):
+        """Added supplier exists and can be searched"""
+        supplier = Supplier.objects.get(companyname="Test company")
+        self.assertEqual(supplier.address, "Kultatie 1")
+        self.assertEqual(supplier.country, "Finland")
+
+class ProductModelTests(TestCase):
+    def setUp(self):
+        Product.objects.create(productname="Hillo", packagesize="300g", unitprice=4.1, unitsinstock=100, companyname="Sokeriveljet")
+        
+    def test_added_product_exists(self):
+        """Added product exists and can be searched"""
+        product = Product.objects.get(unitprice=4.1)
+        self.assertEqual(product.productname, "Hillo")
+
 
 class LaskinTests(TestCase):
     def test_plus(self):
@@ -22,5 +59,7 @@ class LaskinTests(TestCase):
         self.assertEqual(plus('7', '2'), '9')
         self.assertEqual(plus(7.1, 2.1), 9.2)
 
-    # TDD - Test Driven Development
-    '''testi, toiminto, refaktotointi, testi, ... uudistus, testi'''
+    '''
+    TDD - Test Driven Development
+    testi, toiminto, refaktotointi, testi, ... uudistus, testi
+    '''
